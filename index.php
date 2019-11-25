31b61bf2e0d878548c229c3496cc30a3ce86e6ae
gabber78 <gabor.rab@gmail.com>
2019-11-25 16:54:54 +0100

    a
    a

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<?php
 
-  </body>
-</html>
+include 'header.php';
+
+ ?>
+
+    <main>
+
+    </main>
+
+
+
+ <?php include 'footer.php'; ?>
